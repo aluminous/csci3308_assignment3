4a88851e9554ab78bf02b18e956e35e4e760b7b8
Austin Brown <austin@alumino.us>
2013-10-09 13:42:17 -0700

    Another attempt at fixing Heroku issues

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   def self.list_ratings
-    Movie.unscoped.group(:rating).collect { |movie| movie.rating }
+    Movie.select(:rating).group(:rating).collect { |movie| movie.rating }
   end
 end
